9d1c6be448b66db78ab5c363b2340f577205a890
Tai <tai.sycheva@gmail.com>
2019-06-14 20:43:45 +0300

    order food

---
+++ nuxt/nuxttest-master/components/PlaceOrder.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <a href="#">check</a>
+  </div>
+</template>
+
+<script>
+<style scoped>
+.container {
+  width: 30%;
+  background-color: yellow;
+}
+</style>
+</script>
